ec2b930c3ddec3fd9734272c519641152623ab11
Erioni <erionn.ademi@gmail.com>
2024-01-06 14:00:15 +0100

    Added custom comparator for sorting

---
+++ Java Collections/src/sorting/Product.java
@@ -0,0 +1,13 @@
+package sorting;
+
+public interface Product {
+	int getId();
+
+	String getProductName();
+	
+	String getCategoryName();
+	
+	double getPrice();
+
+	void setPrice(double price);
+}
