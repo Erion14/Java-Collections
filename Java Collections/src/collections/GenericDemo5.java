590e6dff2b1856038bc39b37b1e98fb9de0101ff
Erion Ademi <117570706+Erion14@users.noreply.github.com>
2024-01-13 17:18:37 +0100

    Created GenericDemo5

---
+++ Java Collections/src/collections/GenericDemo5.java
@@ -0,0 +1,18 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericDemo5 {
+	public static void main(String[] args) {
+		List<Integer> integers = new ArrayList<>();
+		
+		List list = new ArrayList();
+		list = integers;
+		list.add("some string");
+		
+		Integer integer = integers.get(0);
+		System.out.println(integer);
+		
+	}
+}
